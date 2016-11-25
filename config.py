6f2ce13020e05785131394d5ccb7d40ab18f222e
NanJunLee <NanJun05@163.com>
2016-11-25 13:18:41 +0800

    modify some detail

--- config.py
+++ config.py
@@ -13,9 +13,9 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Pandamen]'
+    FLASKY_MAIL_SENDER = 'Padanmen Admin <Pandamen@example.com>'
+    FLASKY_ADMIN = os.environ.get('Pandamen_ADMIN')
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
     FLASKY_COMMENTS_PER_PAGE = 30
